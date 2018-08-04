fbfadbba7de4e105deb9b1d356cb33d703b88846
kenny <kanishkan14@gmail.com>
2018-08-04 15:50:22 +0530

    fix

--- ccawebsite/settings.py
+++ ccawebsite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '!=7=+0-o&6lcl^-fn#u7h)i^e)1)^)s@s12=x3rcvgtfyu-o*g'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['five-5.herokuapp.com']
 
 
 # Application definition
